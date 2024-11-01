f54248b39f31e596231bb0f4757ea4415b9ae734
coffee-compiler <coffee-compiler@vrapped.de>
2024-11-01 19:20:36 +0100

    add book configuration

---
+++ Media.Api/Data/Configurations/BookConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Media.Api.Domain.Books;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Media.Api.Data.Configurations;
+
+public sealed class BookConfiguration : IEntityTypeConfiguration<Book>
+{
+    public void Configure(EntityTypeBuilder<Book> builder)
+    {
+        builder.HasKey(b => b.Id);
+
+        builder.Property(b => b.Author)
+            .IsRequired();
+
+        builder.Property(b => b.Title)
+            .IsRequired();
+    }
+}
