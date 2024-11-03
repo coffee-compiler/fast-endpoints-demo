26aa899e61cf1a0ec55dc2ee1f24a66552313843
coffee-compiler <coffee-compiler@vrapped.de>
2024-11-03 11:42:41 +0100

    add ping endpoint

---
+++ Media.Api/Features/Misc/Ping/PingEndpoint.cs
@@ -0,0 +1,17 @@
+﻿using FastEndpoints;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace Media.Api.Features.Misc.Ping;
+
+public sealed class PingEndpoint
+    : EndpointWithoutRequest<NoContent>
+{
+    public override void Configure()
+    {
+        Post("/misc/ping");
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(CancellationToken ct)
+        => await SendNoContentAsync(ct);
+}
